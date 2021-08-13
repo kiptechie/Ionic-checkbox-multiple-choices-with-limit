26897a7ac01f01dd1af1d05a8f2b14a1f45409e1
kiptechie <kipkorirtimserem@outlook.com>
2021-08-13 10:57:42 +0300

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -9,32 +9,32 @@ export class HomePage {
   questions = [
     {
       id: 1,
-      question: 'What is the capital of India?',
+      question: 'What is the time now?',
       max_choices: 2,
       choices: [
-        { id: 1, text: 'New Delhi', isChecked: false, disabled: false },
-        { id: 2, text: 'Delhi', isChecked: false, disabled: false },
-        { id: 3, text: 'Mumbai', isChecked: false, disabled: false },
-        { id: 4, text: 'Kolkata', isChecked: false, disabled: false }
+        { id: 1, text: 'Morning', isChecked: false, disabled: false },
+        { id: 2, text: 'Afternoon', isChecked: false, disabled: false },
+        { id: 3, text: 'Noon', isChecked: false, disabled: false },
+        { id: 4, text: 'Evening', isChecked: false, disabled: false }
       ]
     },
 
     {
       id: 2,
-      question: 'What is the size of India?',
+      question: 'What is the capital of Kenya?',
       max_choices: 3,
       choices: [
-        { id: 1, text: 'New ', isChecked: false, disabled: false },
-        { id: 2, text: 'Old', isChecked: false, disabled: false },
-        { id: 3, text: 'Medium', isChecked: false, disabled: false },
-        { id: 4, text: 'Large', isChecked: false, disabled: false }
+        { id: 1, text: 'Kisumu ', isChecked: false, disabled: false },
+        { id: 2, text: 'Nairobi', isChecked: false, disabled: false },
+        { id: 3, text: 'Eldoret', isChecked: false, disabled: false },
+        { id: 4, text: 'Mombasa', isChecked: false, disabled: false }
       ]
     }
   ];
 
   constructor(public navCtrl: NavController) {}
 
-  validateCheckbox(question: any, choice: any) {
+  validateCheckbox(question: any) {
     const questions = this.questions;
     let validatedQuestions = [];
     for (let i = 0; i < questions.length; i++) {
